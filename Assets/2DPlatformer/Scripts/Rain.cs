ccb9444e2e2e2d7a0e642e3d3747ea77dcb0e4ad
userinfamous <ianimate39@gmail.com>
2020-02-02 17:00:11 +0700

    core game works

---
+++ Assets/2DPlatformer/Scripts/Rain.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rain : MonoBehaviour
+{
+    private float timeToDamage;
+    public float startTimeToDamage;
+    private Animator animator;
+    public int damage;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timeToDamage <= 0)
+        {
+           GetComponent<House>().TakeDamage(damage);
+           timeToDamage = startTimeToDamage;
+           Debug.Log("House damaged.");
+        }
+        else
+        {
+            timeToDamage -= Time.deltaTime;
+        }
+
+    }
+}
